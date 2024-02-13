8ab226bf51e84b9d482dc2b0f1f9ad3fe40dd044
Nita <1581096163@qq.com>
2024-02-13 14:15:20 +0800

    feat:滚动条控件

--- WPFTest/NitaCustomControl/NitaTabControl.cs
+++ WPFTest/NitaCustomControl/NitaTabControl.cs
@@ -5,19 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using WPFTest.DataBinding;
 using WPFTest.Enity;
 
 namespace WPFTest.NitaCustomControl
 {
+    [TemplatePart(Name = "PART_ScrollViewer", Type = typeof(ScrollViewer))]
     public class NitaTabControl: TabControl
     {
+        #region Fields
+        private ScrollViewer _scrollViewer;
+        private Visibility lastVerticalScrollBarVisibility = Visibility.Collapsed;
+        #endregion
+
+        #region Construcotrs
         static NitaTabControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(NitaTabControl),
                 new FrameworkPropertyMetadata(typeof(NitaTabControl)));
         }
+        #endregion
 
         #region DependencyProperty
         #region Orientation
@@ -29,6 +38,17 @@ namespace WPFTest.NitaCustomControl
         public static readonly DependencyProperty OrientationProperty =
             DependencyProperty.Register("Orientation", typeof(Orientation), typeof(NitaTabControl), new PropertyMetadata());
         #endregion
+
+        #region AutoScroll
+        public bool AutoScroll
+        {
+            get { return (bool)GetValue(AutoScrollProperty); }
+            set { SetValue(AutoScrollProperty, value); }
+        }
+        public static readonly DependencyProperty AutoScrollProperty =
+            DependencyProperty.Register("AutoScroll", typeof(bool), typeof(NitaTabControl), new PropertyMetadata(false));
+        #endregion
+
         #endregion
 
         #region Override方法
@@ -36,9 +56,45 @@ namespace WPFTest.NitaCustomControl
         {
             return new NitaTabItem();
         }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _scrollViewer = GetTemplateChild("PART_ScrollViewer") as ScrollViewer;
+            if (_scrollViewer == null)
+            {
+                throw new Exception("Hey！ PART_ScrollViewer从模板中丢失，或者不是NitaClickCanvas。抱歉，但是需要此Grid。");
+            }
+           // _scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+        }
         #endregion
 
+        #region Events
 
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double delta = e.Delta;
+            double offset = _scrollViewer.VerticalOffset;
+            double newOffset = offset - delta * 0.1;
+            _scrollViewer.ScrollToVerticalOffset(newOffset);
+            e.Handled = true;
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var currentVerticalScrollBarVisibility = _scrollViewer.ComputedVerticalScrollBarVisibility;
+            if (currentVerticalScrollBarVisibility == Visibility.Visible
+                && lastVerticalScrollBarVisibility != Visibility.Visible)
+            {
+                _scrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
+            }
+            else if (currentVerticalScrollBarVisibility != Visibility.Visible
+                && lastVerticalScrollBarVisibility == Visibility.Visible)
+            {
+                _scrollViewer.PreviewMouseWheel -= ScrollViewer_PreviewMouseWheel;
+            }
+            lastVerticalScrollBarVisibility = currentVerticalScrollBarVisibility;
+        }
+        #endregion
 
     }
 }
